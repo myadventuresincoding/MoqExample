1e88431948debed0b1bcbd35d5727b27acde5c81
myadventuresincoding <66219262+myadventuresincoding@users.noreply.github.com>
2020-05-31 17:23:19 -0600

    Add additional comments explaining test scenarios

--- MoqExample.Tests/SomeAppTests.cs
+++ MoqExample.Tests/SomeAppTests.cs
@@ -8,22 +8,19 @@ namespace MoqExample.Tests
     [TestFixture]
     public class SomeAppTests
     {
-        private Mock<ISomeService> _mockSomeService;
-
-        [SetUp]
-        public void Setup()
-        {
-            _mockSomeService = new Mock<ISomeService>();
-        }
-
-        /// <summary>
+       
         /// Scenario: Return a different item the second time a mocked method is called.
         /// Let's say I have a call in a service that returns the next item to process, 
-        /// such as pulling in customer requests from a queue, and returning them in order.
+        /// such as pulling in customer requests from a queue, and returning them in order. 
+
+        /// <summary>
+        /// The more well known approach is to just use a Queue and have the mock call 
+        /// Dequeue and return the result each time the mocked method is called.
         /// </summary>
         [Test]
         public void MogMethodThatReturnsADifferentValueWhenCalledASecondTime()
         {
+            Mock<ISomeService> _mockSomeService = new Mock<ISomeService>();
             var queueStuff = new Queue<SomeStuff>();
             queueStuff.Enqueue(new SomeStuff { Id = 1, Name = "Real" });
             queueStuff.Enqueue(null);
@@ -35,14 +32,39 @@ namespace MoqExample.Tests
         }
 
         /// <summary>
+        /// The alternative is to use a feature in Moq called Sequences which allows 
+        /// you to set multiple return values, that will be returned one at a time in order, 
+        /// each time the mocked method is called.
+        /// </summary>
+        [Test]
+        public void MogMethodThatReturnsADifferentValueWhenCalledASecondTimeUsingSequences()
+        {
+            Mock<ISomeService> _mockSomeService = new Mock<ISomeService>();
+
+            _mockSomeService.SetupSequence(x => x.GetNextStuff())
+                    .Returns(new SomeStuff { Id = 1, Name = "Real" })
+                    .Returns(null);
+
+            Assert.IsNotNull(_mockSomeService.Object.GetNextStuff());
+            Assert.IsNull(_mockSomeService.Object.GetNextStuff());
+        }
+
         /// Scenario: Throw an exception the first time a mocked method is called, but succeed on the second try.
         /// Lets say for example I want my application to retry on an exception, 
         /// and if it suceeds on the second try, then process the request and keep going.
+
+
+        /// <summary>
+        /// In this case I want to test that my application will handle the case that when a call to a service 
+        /// throws an exception, it will retry and if it receives a valid response on the second try, process the 
+        /// request successfully and continue. You can accomplish this by using a feature in Moq called “Callback”.
         /// </summary>
         [Test]
         public void MogMethodThatThrowsAnExceptionFirstTimeCalledAndAnObjectWithSecondTime()
         {
+            Mock<ISomeService> _mockSomeService = new Mock<ISomeService>();
             var calls = 0;
+
             _mockSomeService.Setup(x => x.GetNextStuff())
                 .Returns(() => new SomeStuff())
                 .Callback(() =>
@@ -56,9 +78,40 @@ namespace MoqExample.Tests
             Assert.IsNotNull(_mockSomeService.Object.GetNextStuff());
         }
 
+        /// <summary>
+        /// Now, it is true that you can use a Moq Sequence to return a different value each time a mocked method 
+        /// is called, but as far as I can tell you can only use this where the valid value is first and throwing 
+        /// an exception is the last item in the sequence. In my case above I explicitly wanted to test that an 
+        /// exception was thrown on the first call and a valid value was returned on the second call. However, 
+        /// if all you need to test in your code is how it handles a valid value on the first call and an exception 
+        /// being thrown on the second call, you can use a Sequence for your mock setup.
+        /// </summary>
+        [Test]
+        public void MockMethodThatReturnsAnObjectFirstTimeCalledAndThrowsAnExceptionSecondTimeUsingSequences()
+        {
+            Mock<ISomeService> _mockSomeService = new Mock<ISomeService>();
+
+            _mockSomeService.SetupSequence(x => x.GetNextStuff())
+            .Returns(new SomeStuff())
+            .Throws<Exception>();
+
+            Assert.IsNotNull(_mockSomeService.Object.GetNextStuff());
+            Assert.Throws<Exception>(() => _mockSomeService.Object.GetNextStuff());
+        }
+
+        /// Scenario: Mock a void method to throw an exception
+
+        /// <summary>
+        /// Lets say I have some void method that normally just silently does some task for me and has no need 
+        /// to have a return type, such as a call to write a stat or a log entry.However, if I want to test how 
+        /// my application handles the case when this call throws an exception, I can use the following setup to 
+        /// mock this method.
+        /// </summary>
         [Test]
         public void MogMethodThatThrowsAnExceptionIsVoidAssertExceptionIsThrown()
         {
+            Mock<ISomeService> _mockSomeService = new Mock<ISomeService>();
+
             _mockSomeService.Setup(x => x.DoStuff()).Throws(new Exception("Failure"));
 
             Assert.Throws<Exception>(() => _mockSomeService.Object.DoStuff());
